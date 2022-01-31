19d13941fa6afdb268289ba258ef2fb37b0f1688
Prince Panja <prince.panja121@gmail.com>
2022-01-31 17:34:10 +0530

    comments added

--- project/myJS.js
+++ project/myJS.js
@@ -1,3 +1,4 @@
+//function to play the sound and start the animatiob
 window.addEventListener('keydown', e =>{
     const audio = document.querySelector(`audio[data-keys="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-keys="${e.keyCode}"]`);
@@ -8,12 +9,14 @@ window.addEventListener('keydown', e =>{
     
 })
 
+//function to attach a stop-transition function to all the keys
 const keys = document.querySelectorAll('.key');
 console.log(keys);
 keys.forEach(key => {
     key.addEventListener('transitionend', stopF);
 })
 
+//function to stop the animation- stop-transition
 function stopF(e){
     if(e.propertyName !== 'transform') return;
     console.log(this);
